50db277d84094a22b54b751f2d3287bf06e2f1c8
a-w-m <mamrey@gmail.com>
2021-04-03 11:43:23 -0400

    testing weight option

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -18,7 +18,7 @@ interface Props {
 }
 
 const BuyButton: React.FC<Props> = (props: Props) => {
-  return <Button className="snipcart-add-item" {...props} data-item-weight = {.12}></Button>
+  return <Button className="snipcart-add-item" {...props} data-item-weight = {200}></Button>
 }
 
 export default BuyButton
