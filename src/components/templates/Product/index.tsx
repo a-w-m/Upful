acda6e2dddc765bbb6c78c049eb949fe4ebeacb0
a-w-m <mamrey@gmail.com>
2021-04-04 18:06:03 -0400

    change netlify function to js

--- src/components/templates/Product/index.tsx
+++ src/components/templates/Product/index.tsx
@@ -51,7 +51,7 @@ const Product: React.FC<P.Product> = ({ data }) => {
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(
-        `https://thirsty-blackwell-f130f4.netlify.app/.netlify/functions/getProductQuantity?id=${id}`
+        `https://shipping--thirsty-blackwell-f130f4.netlify.app/.netlify/functions/getProductQuantity?id=${id}`
       )
       const data = res.json()
       console.log(res)
