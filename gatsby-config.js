edf18a6753986e02f58745c2bb8222d2b1cb413a
a-w-m <47095419+mamrey@users.noreply.github.com>
2021-04-06 14:00:12 -0400

    fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/upfull.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/Upfull.jpg`, // This path is relative to the root of the site.
       },
     },
 
