7b05140be216577e736f5ec7ed993e3dc75e0f84
a-w-m <mamrey@gmail.com>
2021-05-24 16:01:59 -0400

    replace url with ssl url

--- static/functions/deploy-succeeded.js
+++ static/functions/deploy-succeeded.js
@@ -1,12 +1,12 @@
 const GATSBY_SECRET_API = process.env.GATSBY_SECRET_API_KEY 
 const secret = (Buffer.from(GATSBY_SECRET_API).toString('base64'))
 const fetch = require('node-fetch')
-const { parseJsonText } = require('typescript')
 
 exports.handler = async function (event, context){
     const body = JSON.parse(event.body)
-    console.log(event.body, body)
-    const fetchUrl = `${body.payload.url}/${body.payload.title}`
+    const fetchUrl = `${body.payload.ssl_url}/${body.payload.title}`
+    console.log(fetchurl)
+
 
     fetch("https://app.snipcart.com/api/products", {
         method: "post",
