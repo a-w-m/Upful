0248bea4d07924d30e3fd90d824122743eafd38b
a-w-m <mamrey@gmail.com>
2021-05-24 15:17:36 -0400

    declare fetchUrl from event.body - change cms deploy branch to shipping

--- static/functions/deploy-succeeded.js
+++ static/functions/deploy-succeeded.js
@@ -3,7 +3,7 @@ const secret = (Buffer.from(GATSBY_SECRET_API).toString('base64'))
 const fetch = require('node-fetch')
 
 exports.handler = async function (event, context){
-    console.log(event)
+    const fetchUrl = `${event.body.payload.deploy_ssl_url}/${event.body.payload.title}`
 
     fetch("https://app.snipcart.com/api/products", {
         method: "post",
@@ -11,7 +11,7 @@ exports.handler = async function (event, context){
             Authorization: `Basic ${secret}`,
             Accept: "application/json",
           },
-        body: "{fetchUrl: ''}",
+        body: `{fetchUrl: ${fetchUrl}}`,
         
     })
 
