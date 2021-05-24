b64728cf357d7eecf97a7b1e5d13e3e574466314
a-w-m <mamrey@gmail.com>
2021-05-24 16:38:07 -0400

    debugging payload

--- static/functions/deploy-succeeded.js
+++ static/functions/deploy-succeeded.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 exports.handler = async function (event, context){
     const body = JSON.parse(event.body)
     const fetchUrl = `${body.payload.ssl_url}/${body.payload.title}`
-    console.log(fetchUrl)
+    console.log(body)
 
 
     fetch("https://app.snipcart.com/api/products", {
