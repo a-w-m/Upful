f127f119dc3da2a71af57563016d46129d6ab6bf
a-w-m <mamrey@gmail.com>
2021-05-24 21:12:28 -0400

    Update product post request

--- static/functions/deploy-succeeded.js
+++ static/functions/deploy-succeeded.js
@@ -9,22 +9,23 @@ exports.handler = async function (event, context){
     const slug = arr[arr.length-1]
     const fetchUrl = `${body.payload.deploy_ssl_url}/${slug}`
     console.log(fetchUrl, body)
+    const raw = {fetchUrl}
 
 
     fetch("https://app.snipcart.com/api/products", {
-        method: "post",
+        method: "POST",
         headers:{
             Authorization: `Basic ${secret}`,
             Accept: "application/json",
           },
-        body: `{fetchUrl:${fetchUrl}}`,
+        body: JSON.stringify(raw),
         
     }).then(res=>{
         res.json()
     }).then(json=>{
         console.log(json)
-    })
+    }).catch(error => console.log('error', error))
 
-
+    console.log("TEST")
 }
 
