6334961c42303363910eb6ce33d05f3f00c8522f
a-w-m <mamrey@gmail.com>
2021-05-24 21:31:18 -0400

    update fetch promise chain

--- static/functions/deploy-succeeded.js
+++ static/functions/deploy-succeeded.js
@@ -1,31 +1,33 @@
-const GATSBY_SECRET_API = process.env.GATSBY_SECRET_API_KEY 
-const secret = (Buffer.from(GATSBY_SECRET_API).toString('base64'))
-const fetch = require('node-fetch')
+const GATSBY_SECRET_API = process.env.GATSBY_SECRET_API_KEY
+const secret = Buffer.from(GATSBY_SECRET_API).toString("base64")
+const fetch = require("node-fetch")
 
-exports.handler = async function (event, context){
+exports.handler = async function (event, context) {
+  const body = JSON.parse(event.body)
+  const arr = body.payload.title.split(" ")
+  const slug = arr[arr.length - 1]
+  const fetchUrl = `${body.payload.deploy_ssl_url}/${slug}`
+  console.log(fetchUrl, body)
+  const raw = { fetchUrl }
 
-    const body = JSON.parse(event.body)
-    const arr = body.payload.title.split(" ")
-    const slug = arr[arr.length-1]
-    const fetchUrl = `${body.payload.deploy_ssl_url}/${slug}`
-    console.log(fetchUrl, body)
-    const raw = {fetchUrl}
+  return fetch("https://app.snipcart.com/api/products", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${secret}`,
+      Accept: "application/json",
+    },
+    body: JSON.stringify(raw),
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log(data)
+      return result
+    })
+    .then(result => ({
+      statusCode: 200,
+      body: "Product Sent to Inventory Dashboard",
+    }))
+    .catch(error => console.log("error", error))
 
-
-    fetch("https://app.snipcart.com/api/products", {
-        method: "POST",
-        headers:{
-            Authorization: `Basic ${secret}`,
-            Accept: "application/json",
-          },
-        body: JSON.stringify(raw),
-        
-    }).then(res=>{
-        res.json()
-    }).then(json=>{
-        console.log(json)
-    }).catch(error => console.log('error', error))
-
-    console.log("TEST")
+  console.log("TEST")
 }
-
