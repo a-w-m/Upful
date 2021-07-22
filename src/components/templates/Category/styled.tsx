0cfa9c21ba1803929bd6fcd8aa2357e388a3cc41
a-w-m <mamrey@gmail.com>
2021-07-22 18:32:23 -0400

    Responsive product grid large device

--- src/components/templates/Category/styled.tsx
+++ src/components/templates/Category/styled.tsx
@@ -1,14 +1,22 @@
 import styled from "styled-components"
+import device from "../../layout/mediaQuery"
 
 export const CategoryHeading = styled.h1`
   font-size: var(--heading-font-size);
   font-family: var(--main-font-size);
-  padding-bottom: 2rem;
+  font-weight: 300;
   text-align: center;
+  margin: 0 auto;
+  padding: 1rem 1rem;
+
 `
 
 export const Container = styled.div`
   display: grid;
   grid-auto-rows: auto;
-  grid-gap: 1rem;
+  grid-gap: 2rem;
+
+  @media ${device.laptop}{
+    grid-gap: 4rem;
+  }
 `
