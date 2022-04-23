2dd6ed5e0bd53d7731cff79ee5d3e7536b6164be
a-w-m <mamrey@gmail.com>
2022-04-22 20:45:35 -0400

    Get favicon from metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Upfull.jpg`, // This path is relative to the root of the site.
+        icon: metadata.favicon.replace("/", ""),  // set favicon icon, this path is relative to the root of the site.
       },
     },
 
