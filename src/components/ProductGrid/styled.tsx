d3d6379e4d7c604ee57e28bd4ef00c596281fd1f
a-w-m <mamrey@gmail.com>
2022-05-06 16:29:05 -0400

    Center orphan grid items

--- src/components/ProductGrid/styled.tsx
+++ src/components/ProductGrid/styled.tsx
@@ -4,13 +4,13 @@ import device from "../Layout/mediaQuery"
 export const Container = styled.div`
   display: grid;
   width: auto;
-  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  grid-template-columns: repeat(4, 1fr);
   padding: 2rem 0;
   grid-gap: 2rem;
   justify-content: center;
 
-@media ${device.laptop} {
-  grid-template-columns: repeat(auto-fit, minmax(300px, .25fr));
+@media ${device.tablet} {
+  grid-template-columns: repeat(6, 1fr);
   padding: 2rem 2rem;
 }
 `
--- src/components/ProductThumbnail/styled.tsx
+++ src/components/ProductThumbnail/styled.tsx
@@ -1,9 +1,47 @@
 import styled from "styled-components"
-interface Props{
+import device from "../Layout/mediaQuery"
+interface Props {
   stock: number
 }
 
-export const Article = styled.article``
+export const Article = styled.article`
+  grid-column: span 2;
+  /* @media ${device.tablet}{
+    width: 400px;
+  } */
+
+  /* handle widow/orphan grid items: https://css-irl.info/controlling-leftover-grid-items/ */
+  
+  /* Dealing with single possible orphan on mobileS, M, L, 4 column grid */
+  &:last-child:nth-child(2n-1){
+    grid-column-end: -2;
+    @media ${device.tablet}{
+      grid-column-end: auto;
+    }
+  }
+
+
+ /* Dealing with orphan grid items for tablet, laptop, and laptopL, 6 column grid */
+ /* Dealing with 2 orphan items */
+
+  &:last-child:nth-child(3n - 1) {
+    @media ${device.tablet} {
+      grid-column-end: -2;
+    }
+  }
+  &:nth-last-child(2):nth-child(3n + 1) {
+    @media ${device.tablet} {
+      grid-column-end: 4;
+    }
+  }
+  /* Dealing with single orphan */
+
+  &:last-child:nth-child(3n - 2) {
+    @media ${device.tablet} {
+      grid-column-end: 5;
+    }
+  }
+`
 export const H3 = styled.h3`
   font-size: 1.5rem;
   line-height: 1.25;
@@ -13,12 +51,10 @@ export const PriceWrapper = styled.div<Props>`
   font-size: 1.6rem;
   color: #000;
   opacity: 0.5;
-  text-decoration: ${props=>props.stock>0 ? "none": "line-through"};
+  text-decoration: ${props => (props.stock > 0 ? "none" : "line-through")};
 `
 
-export const SoldOut = styled.span`
-  
-`
+export const SoldOut = styled.span``
 
 export const ProductInfoContainer = styled.div`
   display: flex;
