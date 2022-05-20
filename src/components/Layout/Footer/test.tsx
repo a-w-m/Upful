808b6b83ccd2a402b2ac193b294a523b361cc89c
a-w-m <mamrey@gmail.com>
2022-05-20 18:26:50 -0400

    Footer Test

--- src/components/Layout/Footer/test.tsx
+++ src/components/Layout/Footer/test.tsx
@@ -1,10 +1,32 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import Footer from "."
 
-describe("Footer Component", () => {
-  test("matches component", () => {
-    const { container } = render(<Footer />)
-    expect(container.firstElementChild).toMatchSnapshot()
+describe("Footer", () => {
+  beforeEach(()=>{
+    render (<Footer/>)
   })
+
+  it(" should match snapshot", () => {
+    const footer = screen.getByRole('contentinfo')
+    expect(footer).toMatchSnapshot()
+  })
+
+  it("should display site links", ()=>{
+    const nav = screen.getByRole('navigation')
+    
+    const menuLinks = within(nav).getAllByRole('link')
+    expect(menuLinks).toHaveLength(4)
+  })
+
+  it("should display email address link", ()=>{    
+    const email = screen.getByRole('link', {name: /howdy@storefront.com/})
+    expect(email).toBeInTheDocument()
+  })
+
+  it("should display copyright text", ()=>{
+    const copyright = screen.getByText(/© Upful 2022 Made with full Irations/)
+    expect(copyright).toBeInTheDocument()
+  })
+
 })
