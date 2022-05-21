342e43dfe02a3284ac2b394f2903ab9c9cb13419
a-w-m <mamrey@gmail.com>
2022-05-21 09:32:11 -0400

    Layout Test

--- src/components/Layout/test.tsx
+++ src/components/Layout/test.tsx
@@ -1,19 +1,41 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import Layout from "."
+import {SnipcartContextProvider} from "test-utils"
 
-describe("Layout component", () => {
-  test("should match snapshot", () => {
-    render(<Layout children="" />)
-    const { container } = render(<Layout children="" />)
 
+describe("Layout", () => {
+  const child = <h1>child</h1>
+  beforeEach(()=>{
+  render(<Layout children= {child}/>, {wrapper: SnipcartContextProvider})
+  })
+
+  it("should match snapshot", () => {
+    const {container} = render(<Layout children={child} />, {wrapper: SnipcartContextProvider})
     expect(container.firstChild).toMatchSnapshot()
+
   })
 
-  test("should display correct title and date", () => {
-    render(<Layout children="" />)
-
-    expect(screen.getByText("StoreFront")).toBeInTheDocument()
-    expect(screen.getByText(/2019/)).toBeInTheDocument()
+  it("should display header", ()=>{
+    const header = screen.getByRole('banner')
+    expect(header).toBeInTheDocument()
   })
+
+  it("should display footer", ()=>{
+    const header = screen.getByRole('contentinfo')
+    expect(header).toBeInTheDocument()
+  })
+
+  
+  it("should display main", ()=>{
+    const header = screen.getByRole('main')
+    expect(header).toBeInTheDocument()
+  })
+
+  it("should display children", ()=>{
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument()
+  })
+
+  
 })
