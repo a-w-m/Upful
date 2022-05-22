dfcb49208bae869cfa259be0a1dc70b159bf0e61
a-w-m <mamrey@gmail.com>
2022-05-22 11:16:35 -0400

    Pagination Test - case insensitive regex

--- src/components/Pagination/test.tsx
+++ src/components/Pagination/test.tsx
@@ -24,31 +24,31 @@ describe("Pagination", () => {
 
   it("should not display previous page link when on first page", () => {
     render(<Pagination {...mockData} />)
-    const previousLink = screen.queryByRole("link", { name: /previous/ })
+    const previousLink = screen.queryByRole("link", { name: /previous/i })
     expect(previousLink).toBeNull()
   })
 
   it("should link to category page when the previous page is the first page", () => {
     render(<Pagination {...mockData} currentPage={mockData.currentPage+1} />)
-    const previous = screen.getByRole("link", { name: /previous/ })
+    const previous = screen.getByRole("link", { name: /previous/i })
     expect(previous).toHaveAttribute('href', `/${mockData.category}/`)
   })
 
   it("should link to previous numbered page when the previous page is not the first page", () => {
     render(<Pagination {...mockData} currentPage={mockData.numPages} />)
-    const previous = screen.getByRole("link", { name: /previous/ })
+    const previous = screen.getByRole("link", { name: /previous/i })
     expect(previous).toHaveAttribute('href', `/${mockData.category}/${mockData.numPages-1}/`)
   })
 
   it("should not display next page link when on last page", () => {
     render(<Pagination {...mockData} currentPage={mockData.numPages} />)
-    const next = screen.queryByRole("link", {name: /next/})
+    const next = screen.queryByRole("link", {name: /next/i})
     expect(next).toBeNull()
   })
 
   it("should link to the next page when not on last page", ()=>{
     render(<Pagination {...mockData} />)
-    const next = screen.queryByRole("link", {name: /next/})
+    const next = screen.queryByRole("link", {name: /next/i})
     expect(next).toHaveAttribute('href', `/${mockData.category}/${mockData.currentPage+1}/`)
 
   })
