951c1da42903477c608249c1f87a8d88b8d464dc
a-w-m <mamrey@gmail.com>
2022-05-22 09:29:18 -0400

    Move mock data

--- __mocks__/mock-data.tsx
+++
@@ -1,5 +0,0 @@
-export { getSnipcartApiData, getSnipcartContextData } from "./data/Providers"
-export {getImageGalleryData} from "./data/ImageGallery"
-export {getProductFormData} from "./data/ProductForm"
-
-
