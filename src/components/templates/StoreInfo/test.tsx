01414faab953f9bb2a5de4d24b696e77edae17f4
a-w-m <mamrey@gmail.com>
2022-05-27 12:24:12 -0400

    StoreInfo test

--- src/components/templates/StoreInfo/test.tsx
+++ src/components/templates/StoreInfo/test.tsx
@@ -1,25 +1,34 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "src/utils/test/test-utils"
 import Markdown from "./index"
 const mockData = {
   data: {
-    file: {
-      childMarkdownRemark: {
-        html: "<p>TEST</p>",
+    markdownRemark: {
+      frontmatter:{
+        title: 'TEST'
+      },
+      html: '<p>html</p>'
       },
     },
-  },
-}
+  }
 
-describe("Markdown component", () => {
-  test("snapshot", () => {
+describe("Markdown", () => {
+
+  it("should match snapshot", () => {
     const { container } = render(<Markdown {...mockData} />)
     expect(container.firstChild).toMatchSnapshot()
   })
 
-  test("render html", () => {
+  it("should render html", () => {
     render(<Markdown {...mockData} />)
-    const html = screen.getByText("TEST")
+    const html = screen.getByText("html")
     expect(html).toBeInTheDocument()
+
+  })
+
+  it("should render title", ()=>{
+    render(<Markdown {...mockData} />)
+    const title = screen.getByRole('heading', {level: 1, name: 'TEST'})
+    expect(title).toBeInTheDocument()
   })
 })
