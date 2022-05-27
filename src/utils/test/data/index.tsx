440bdd2ce4044755e4b744447eb2fb558fa6c150
a-w-m <mamrey@gmail.com>
2022-05-27 12:25:11 -0400

    Mock data

--- src/utils/test/data/index.tsx
+++ src/utils/test/data/index.tsx
@@ -2,5 +2,7 @@ export { getSnipcartApiData, getSnipcartContextData } from "./Providers"
 export {getImageGalleryData} from "./ImageGallery"
 export {getProductFormData} from "./ProductForm"
 export {getProductGridData} from "./ProductGrid"
+export {getProductData} from "./Product"
+export {getQueryMockData} from "./queries"
 
 
